478a2d007bd06bc68fe5cdcf24b8deec7cefc4b2
tanthinhdt <tanthinhdt24@gmail.com>
2023-04-19 21:02:28 +0700

    fixed config in models

--- models/t5/t5_small.py
+++ models/t5/t5_small.py
@@ -4,7 +4,7 @@ from transformers import T5Tokenizer, T5Config, T5ForConditionalGeneration
 class T5Small:
     def __init__(self, pretrained_model="t5-small", configs=None):
         self.tokenizer = T5Tokenizer.from_pretrained(pretrained_model)
-        self.configs = T5Config(**configs)
+        self.configs = T5Config(**configs) if configs else T5Config()
         self.model = T5ForConditionalGeneration(self.configs)
         self.model.from_pretrained(pretrained_model)
 
